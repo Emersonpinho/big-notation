b2edd38a5c19a599a96afce7cc8e0455286e7cd2
Emerson <the.emerson.araujo@gmail.com>
2025-02-05 09:49:59 -0300

    corrige comentário na função calc_c_to_f para melhor clareza

--- calculadoraCelParaFahr.js
+++ calculadoraCelParaFahr.js
@@ -1,5 +1,5 @@
 function calc_c_to_f(c) {
-    let f = (c * 9 / 5) + 32
+    let f = (c * 9 / 5) + 32 // tenho uma soma, uma divisão, uma multiplicação, uma tribuição e retornei f. então é igual a 5 operações ou seja, n = 5
     return f;
 }
 
