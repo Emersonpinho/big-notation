5f827b223f3009ca851190fb9c70f8aa0642499a
Emerson <the.emerson.araujo@gmail.com>
2025-02-05 17:10:24 -0300

    corrige lógica da busca binária e adiciona verificação para lista vazia

--- buscaBinaria.js
+++ buscaBinaria.js
@@ -1,27 +1,27 @@
 function busca_binaria(lista, e) {
-    function busca (lista, e, inicio, fim) {
-        if (inicio == fim) {
-            return lista[inicio] == e
-        }
-        let meio = Math.floor((inicio + fim) / 2)
-        if (lista[meio] == e) {
-            return true
-        } else if (lista[meio] > e) {
-            if (inicio == meio) {
-                return false
-            } else {
-                return busca(lista, e, inicio, meio - 1)
-            }
+    function busca(lista, e, inicio, fim) {
+        if (inicio > fim) {
+            return false;
         }
 
-        if (lista.length == 0) {
-            return false
+        const meio = Math.floor((inicio + fim) / 2);
+
+        if (lista[meio] === e) {
+            return true;
+        } else if (lista[meio] < e) {
+            return busca(lista, e, meio + 1, fim);
         } else {
-            return busca(lista, e, 0, lista.length - 1)
+            return busca(lista, e, inicio, meio - 1);
         }
+    }
+
+    if (lista.length == 0) {
+        return false;
+    } else {
+        return busca(lista, e, 0, lista.length - 1);
     }
 }
 
-lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 console.log(`Busca binária(10): ${busca_binaria(lista, 10)}`);
 console.log(`Busca binária(21): ${busca_binaria(lista, 21)}`);
